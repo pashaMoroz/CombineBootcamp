94a97f9bc92e1d63fe936f4b18e77a3db949fd02
Moroz Pavlo <pavlo@sidekickinteractive.com>
2023-01-23 08:29:44 -0500

    Initial Commit

---
+++ CombineApp/CombineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAppApp.swift
+//  CombineApp
+//
+//  Created by Moroz Pavlo on 2023-01-23.
+//
+
+import SwiftUI
+
+@main
+struct CombineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CombineApp
+//
+//  Created by Moroz Pavlo on 2023-01-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
